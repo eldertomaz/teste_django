4ed1ec8d58f0fcebc94a06c50bfc1edb143b7ec1
Elder Tomazinho <admin@qualidadeemsaudesa.onmicrosoft.com>
2025-01-15 17:33:24 -0300

    bug: Nomeclatura dos filters

--- core/apps/user/filters.py
+++ core/apps/user/filters.py
@@ -1,25 +1,25 @@
 import django_filters
 
 from core.apps.base.filters import BaseFilter
-from .models import Technologies, Programmers, Projects, Allocations
+from core.apps.user import models
 
 class TechnologiesFilter(BaseFilter, django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
-        model = Technologies
+        model = models.Technologies
         fields = ['name']
 
 
 class ProgrammersFilter(BaseFilter, django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
     technologies = django_filters.ModelMultipleChoiceFilter(
-        queryset=Technologies.objects.all(),
-        conjoined=True  
+        queryset = models.Technologies.objects.all(),
+        conjoined = True  
     )
 
     class Meta:
-        model = Programmers
+        model = models.Programmers
         fields = ['name', 'technologies']
 
 
@@ -28,20 +28,20 @@ class ProjectsFilter(BaseFilter, django_filters.FilterSet):
     start_date = django_filters.DateFilter(field_name='start_date', lookup_expr='gte')
     end_date = django_filters.DateFilter(field_name='end_date', lookup_expr='lte')
     technologies = django_filters.ModelMultipleChoiceFilter(
-        queryset=Technologies.objects.all(),
+        queryset = models.Technologies.objects.all(),
         conjoined=False  
     )
 
     class Meta:
-        model = Projects
+        model = models.Projects
         fields = ['name', 'start_date', 'end_date', 'technologies']
 
 
 class AllocationsFilter(BaseFilter, django_filters.FilterSet):
-    project = django_filters.ModelChoiceFilter(queryset=Projects.objects.all())
-    programmer = django_filters.ModelChoiceFilter(queryset=Programmers.objects.all())
+    project = django_filters.ModelChoiceFilter(queryset = models.Projects.objects.all())
+    programmer = django_filters.ModelChoiceFilter(queryset = models.Programmers.objects.all())
     hours = django_filters.NumberFilter(lookup_expr='gte')
 
     class Meta:
-        model = Allocations
+        model = models.Allocations
         fields = ['project', 'programmer', 'hours']
